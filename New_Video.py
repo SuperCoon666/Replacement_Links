42e0cdf3d535faddabca75f5c96fb61d6ab80445
SuperCoon <voprishko03@gmail.com>
2021-10-16 18:22:27 +0300

    its work almost right

--- New_Video.py
+++ New_Video.py
@@ -48,6 +48,37 @@ class New_Video(object):
             return name
         except:
             return "Error: "+ str(sys.exc_info()[1])
+    def new(self, source, name):
+        source = source.replace('"', '') + "/videos"
+        pars = requests.get(source)
+        soup = str(BeautifulSoup(pars.text, 'lxml'))
+
+        while True:
+            target = '"title":{"runs":[{"text":' + name
+            ind = soup.index(target)
+            txt = ""
+            c =1
+            while True:
+                x = soup[ind+len(target)+c]
+                if  x== "}":
+                    break
+                txt +=x
+                c+=1
+
+            if "Трансляция закончилась" in txt:
+                soup = soup[ind+len(target)+1:]
+            else:
+                soup = soup[ind+len(target)+1:]
+                res_ind = soup.index('"videoId":')+len('"videoId":')
+                res = soup[res_ind+1:res_ind+11+1]
+                # print(res)
+                break
+
+        # print(soup)
+        # print(name)
+        # print(source)
+        # print(txt)
+        return res
 
     def tst(self, link):
         url = "https://www.youtube.com/watch?v="
@@ -57,5 +88,7 @@ class New_Video(object):
         r1 = self.get_channel(soup)
         r2 = self.get_name(soup)
 
-        res = link, r2, r1
+        # res = [link, r2, r1]
+        # return res
+        res = self.new(r1, r2)
         return res
--- main.py
+++ main.py
@@ -5,24 +5,26 @@ import lxml
 
 from New_Video import New_Video
 from Streams_State import Streams_State
-
-
 # link = "KFHaZAIG2sA"
-link = "-6JmbdjW-M0"
 
-
-
-
-
-state = Streams_State('C:\\Users\\Vlad\\Desktop\\Data_Streams(1).txt')
+state = Streams_State('C:\\Users\\Vlad\\Desktop\\Data_Streams.txt')
 print("Please wait for the result...")
 x = state.get_state()
+# print(x)
 # for i in range(len(x[0])):
 #     print(x[0][i])
 # print("Numbers of not working links:"+x[1])
+# print("")
 
+rr = x[1].split(",")
 new_vid = New_Video()
 for i in range(len(x[2])):
-    lin = x[2][i].replace("'", '')
-    res = new_vid.tst(lin)
-    print(res)
+    try:
+        lin = x[2][i].replace("'", '')
+        res = new_vid.tst(lin)
+        old = rr[i]+": "+"Old link:"+x[2][i]
+        new = "New link:"+res
+        print(old, new)
+    except:
+        print(rr[i]+": "+"We could not find a replacement for this video")
+        print("Error: "+ str(sys.exc_info()[1]))
